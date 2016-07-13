3bc28621044a9f1bcb1ada78c5085b1c9832df79
Steven Tessler <stevenkabc@redwood6.com Primary Public stevenkabc@redwood6.com Primary Public stevenkabc@redwood6.com>
2016-07-12 22:41:28 -0400

    test Todo.js as module

---
+++ src/app/Todo.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import { View, Text, TextInput, TouchableHighlight } from 'react-native';
+
+export class Todo extends Component {
+  render() {
+      return (
+        <Text>Hello</Text>
+      )
+  }
+
+}
