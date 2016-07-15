568f6e194ccc405378d83d746fac052294440fa9
Steven Tessler <stevenkabc@redwood6.com Primary Public stevenkabc@redwood6.com Primary Public stevenkabc@redwood6.com>
2016-07-15 18:16:14 -0400

    fancy

--- index.android.js
+++ index.android.js
@@ -7,10 +7,10 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import {Todo} from './src/app/Todo';
-// import {Fancy} from './src/app/fancy'
+import {Fancy} from './src/app/fancy'
 
-const Main = () => (<Todo />);
-// const Main = () => (<Fancy />);
+// const Main = () => (<Todo />);
+const Main = () => (<Fancy />);
 
 
 AppRegistry.registerComponent('Todo', () => Main);
