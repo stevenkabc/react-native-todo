9988e171ff34cca9df46aa7d7fc0ac18ab96744a
Steven Tessler <stevenkabc@redwood6.com Primary Public stevenkabc@redwood6.com Primary Public stevenkabc@redwood6.com>
2016-07-17 18:51:14 -0400

    vscode support

--- index.android.js
+++ index.android.js
@@ -8,9 +8,10 @@ import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import {Todo} from './src/app/Todo';
 import {Fancy} from './src/app/fancy'
+import {Reddit} from './src/app/Reddit'
 
 // const Main = () => (<Todo />);
-const Main = () => (<Fancy />);
+const Main = () => (<Reddit />);
 
 
 AppRegistry.registerComponent('Todo', () => Main);
